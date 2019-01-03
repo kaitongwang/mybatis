26040521f00f6713f787c2fdd4ca6f3edaa13b4f
Kt.w <kaitong.wang@biz-united.com.cn>
2019-01-03 15:13:45 +0800

    隐藏密码

--- src/main/java/com/mmall/dao/UserMapper.java
+++ src/main/java/com/mmall/dao/UserMapper.java
@@ -3,12 +3,32 @@ package com.mmall.dao;
 import com.lajd.pojo.User;
 
 public interface UserMapper {
+    /**
+     * 根据id删除一条记录
+     * @param id
+     * @return
+     */
     int deleteByPrimaryKey(Long id);
 
+    /**
+     * 无选择的插入一条记录
+     * @param record
+     * @return
+     */
     int insert(User record);
 
+    /**
+     * 有选择的插入一条记录
+     * @param record
+     * @return
+     */
     int insertSelective(User record);
 
+    /**
+     * 根据id查询一条记录
+     * @param id
+     * @return
+     */
     User selectByPrimaryKey(Long id);
 
     int updateByPrimaryKeySelective(User record);
